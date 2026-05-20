dc26fa873e7b5352cb7077665560d05c2fbfb277
Atyla Yuri Torres da Silva <168493753+atylayuri@users.noreply.github.com>
2026-05-20 17:51:19 -0300

    reflexão
    Adição da da atividade 5 reflexão

---
+++ repositorios/repositorio_emprestimo.py
@@ -0,0 +1,65 @@
+# RepositorioEmprestimo: persistência em memória
+
+from modelos.equipamento import Notebook, Projetor, Tablet
+
+
+class RepositorioEmprestimo:
+
+    def __init__(self):
+        # Base inicial simulada
+        self.equipamentos = [
+            Notebook(1, "Notebook Dell", "notebook"),
+            Projetor(2, "Projetor Epson", "projetor"),
+            Tablet(3, "Tablet Samsung", "tablet")
+        ]
+
+        self.emprestimos = []
+
+    # Buscar equipamento por ID
+    def buscar_equipamento(self, equip_id: int):
+        for equipamento in self.equipamentos:
+            if equipamento.id == equip_id:
+                return equipamento
+        return None
+
+    # Salvar novo empréstimo
+    def salvar_emprestimo(self, emprestimo):
+        self.emprestimos.append(emprestimo)
+
+    # Tornar equipamento indisponível
+    def marcar_indisponivel(self, equip_id: int):
+        equipamento = self.buscar_equipamento(equip_id)
+
+        if equipamento:
+            equipamento.disponivel = False
+
+    # Buscar empréstimo por ID
+    def buscar_emprestimo(self, emprestimo_id: int):
+        for emprestimo in self.emprestimos:
+            if emprestimo.id == emprestimo_id:
+                return emprestimo
+        return None
+
+    # Registrar devolução
+    def marcar_devolvido(self, emprestimo_id: int):
+        emprestimo = self.buscar_emprestimo(emprestimo_id)
+
+        if emprestimo:
+            emprestimo.devolvido = True
+
+    # Tornar equipamento disponível novamente
+    def marcar_disponivel(self, equip_id: int):
+        equipamento = self.buscar_equipamento(equip_id)
+
+        if equipamento:
+            equipamento.disponivel = True
+
+    # Buscar empréstimos atrasados
+    def buscar_emprestimos_atrasados(self, hoje):
+        atrasados = []
+
+        for emprestimo in self.emprestimos:
+            if not emprestimo.devolvido and emprestimo.data_devolucao < hoje:
+                atrasados.append(emprestimo)
+
+        return atrasados
